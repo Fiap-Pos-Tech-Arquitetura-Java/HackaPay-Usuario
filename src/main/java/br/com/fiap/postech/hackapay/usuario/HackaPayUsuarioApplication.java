f652ff69e1cf1629f460bf93a45a744fb9e80a8d
Anderson0Wagner <anderson.wagner@gmail.com>
2024-07-29 19:25:01 -0300

    commit inicial do projeto Spring.

---
+++ src/main/java/br/com/fiap/postech/hackapay/usuario/HackaPayUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.hackapay.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaPayUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackaPayUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/postech/hackapay/usuario/HackaPayUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.hackapay.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackaPayUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
