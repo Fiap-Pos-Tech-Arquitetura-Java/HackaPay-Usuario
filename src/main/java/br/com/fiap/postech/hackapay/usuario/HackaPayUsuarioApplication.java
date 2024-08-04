fd486fa4323dde98405802573b893e8cee638d5c
andersonwagner <anderson.wagner@gmail.com>
2024-08-04 10:54:24 +0200

    removendo import

--- src/main/java/br/com/fiap/postech/hackapay/usuario/HackaPayUsuarioApplication.java
+++ src/main/java/br/com/fiap/postech/hackapay/usuario/HackaPayUsuarioApplication.java
@@ -1,7 +1,6 @@
 package br.com.fiap.postech.hackapay.usuario;
 
 import br.com.fiap.postech.hackapay.usuario.entity.User;
-import br.com.fiap.postech.hackapay.usuario.repository.UserRepository;
 import br.com.fiap.postech.hackapay.usuario.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
